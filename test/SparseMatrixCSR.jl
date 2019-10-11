834c5ce948e66b43cdded1ba9d9b75ca41ef2359
victorsndvg <victorsv@gmail.com>
2019-10-11 13:19:30 +0200

    improve coverage

--- test/SparseMatrixCSR.jl
+++ test/SparseMatrixCSR.jl
@@ -10,11 +10,22 @@
         CSC = sparse(I, J, V)
         CSR = sparsecsr(I, J, V)
 
-        @test size(CSC)==size(CSR)==reverse(size(CSR.mat))
+
 
         @test CSC==CSR
 
         @test nnz(CSC) == count(i->(i!=0), CSC) == nnz(CSR) == count(i->(i!=0), CSR)
+
+        TCSC = sparse(J, I, V)
+        TCSR = sparsecsr(J, I, V)
+
+        @test size(CSC)==size(CSR)==reverse(size(CSR.mat))
+        @test size(CSC)==size(CSR)==reverse(size(TCSC))==reverse(size(TCSC))
+
+        @test [nzrange(CSC,col) for col in 1:size(CSC,2)] == [nzrange(TCSR,row) for row in 1:size(TCSR,1)]
+        @test [nzrange(CSR,row) for row in 1:size(CSR,1)] == [nzrange(TCSC,col) for col in 1:size(TCSC,2)]
+
+        @test nonzeros(CSC) == nonzeros(TCSR) && nonzeros(CSR) == nonzeros(TCSC) 
 
         ICSC,JCSC,VCSC= findnz(CSC)
         ICSR,JCSR,VCSR= findnz(CSR)
