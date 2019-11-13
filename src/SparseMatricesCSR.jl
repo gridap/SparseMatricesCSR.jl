69f6722aa50a0f885aa61879dfb4aa7e12e5db76
victorsndvg <victorsv@gmail.com>
2019-11-13 16:10:25 +0100

    import * operator

--- src/SparseMatricesCSR.jl
+++ src/SparseMatricesCSR.jl
@@ -17,6 +17,7 @@ This module contains:
     + `getindices`: Return the indices array of a SparseMatrix (`rowval` or `colval` depending on the SparseMatrix type)
 
   - Overloaded procedures:
+    + `+`: SparseMatrix-Vector product.
     + `mul!`: SparseMatrix-Vector product.
     + `nnz`: Return the number of stored (filled) elements in a sparse array.
     + `nonzeros`: Return `nzval` array.
@@ -30,7 +31,7 @@ module SparseMatricesCSR
 using SparseArrays
 using LinearAlgebra
 
-import Base: size, getindex, show, count
+import Base: size, getindex, show, count, *
 import LinearAlgebra: mul!
 import SparseArrays: nnz, getnzval, nonzeros, nzrange, findnz, rowvals
 
